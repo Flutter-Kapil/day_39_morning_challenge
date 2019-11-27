183447a694f0e97d9ccc1d3c2f1d3319b5f42cd6
kapiljhajhria <Aerospace44>
2019-11-27 10:32:27 +0530

    save 02

--- bin/main.dart
+++ bin/main.dart
@@ -4,33 +4,45 @@
 // input: [[3,4],[1,2],[]]
 // output: [[[1,3,4],[2],[]], [[3,4],[2],[1]], [[4],[1,2],[3]]]
 
-List<List> hanoi(List<List> towers){
-  List<List> towersrsPossibleOutput =[];
+List<List> hanoi(List<List> towers) {
+  List<List> towersPossibleOutput = [];
   //lets assume there are only 3 towers
-  int count=0;
-  int a=1, b=2, c=0;
-  int discToMove = towers[a].first;
-//  print(discToMove);//#debug
-  if(towers[b].isEmpty || discToMove<towers[b].first){
-    towers[a].removeAt(0);
-    towers[b].insert(0,discToMove);
-    print('towers here 1: $towers');//#debug
-    towersrsPossibleOutput.add(List.from(towers).toList());
-    towers[b].removeAt(0);
-    towers[a].insert(0,discToMove);
+  for (int i = 0; i < 3; i++) {
+    if(towers[i].isEmpty){
+      continue;
+    }
+    int discToMove = towers[i].first;
+    //  print(discToMove);//#debug
+    for (int j = 0; j < 3; j++) {
+      if (i == j) {
+        continue;
+      }
+      if (towers[j].isEmpty || discToMove < towers[j].first) {
+        List temp=copyList(towers);
+        towers[i].removeAt(0);
+        towers[j].insert(0, discToMove);
+        print('towers $i: $towers'); //#debug
+        towersPossibleOutput.add(copyList(towers));
+        towers[j].removeAt(0);
+        towers[i].insert(0, discToMove);
+      }
+    }
+  }
 
-  }
-  if(towers[2].isEmpty || discToMove<towers[c].first){
-    towers[a].removeAt(0);
-    towers[c].insert(0,discToMove);
-    print('towers here 2: $towers');//#debug
-    towersrsPossibleOutput.add(List.from(towers).toList());
-    towers[c].removeAt(0);
-    towers[c].insert(0,discToMove);
-  }
-  return towersrsPossibleOutput;
+  //--------------------
+  return towersPossibleOutput;
+}
+
+List copyList(List copyFrom) {
+  List copyTo = [];
+  copyFrom.forEach((item) => copyTo.add(item));
+  return copyTo;
 }
 
 main() {
-  print(hanoi([[3,4],[1,2],[]]));
+  print(hanoi([
+    [3, 4],
+    [1, 2],
+    []
+  ]));
 }
